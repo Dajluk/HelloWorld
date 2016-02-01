38b4788abd174f9e9fd93fee1aae6db8e794e327
Dajluk <kijanka2001@wp.pl>
2016-02-01 23:59:41 +0100

    HelloWorld

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,17 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+/*Nie mam pomys³u co tu napisaæ, wiêc wkleje cytaty Einsteina
+"Wyob­raŸnia jest wa¿niej­sza od wie­dzy, po­niewa¿ wie­dza jest oraniczona."*/
+//"Je¿eli za­ba³aga­nione biur­ko jest oz­nak¹ za­ba³aga­nione­go umys³u, oz­nak¹ cze­go jest pus­te biurko?" 
+using namespace std;
+int main()
+{
+	cout << "Hello World" << endl;
+    return 0;
+}
+
